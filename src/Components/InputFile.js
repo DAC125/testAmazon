9630766b26763d3f0a251e2034a538bcf0677d2f
DAC125 <dcapro170_@hotmail.com>
2022-09-29 20:45:57 -0600

    input file component

---
+++ src/Components/InputFile.js
@@ -0,0 +1,71 @@
+import React, { useRef } from "react";
+import "./../CSS/InputFile.css";
+
+const InputFile = (props) => {
+    
+    const contentBar = useRef(null)
+    const closeBtn = useRef(null)
+    const bar = useRef(null)
+    const input = useRef(null)
+
+    const timeout = (delay) => {
+        return new Promise( res => setTimeout(res, delay) );
+    }
+
+    const handleClick = () =>{
+        contentBar.current.style.display = "none"
+        closeBtn.current.style.display = "none"
+        bar.current.style.width = "0%"
+        input.current.value = null
+        props.uploadFile("")
+
+    }
+
+    const getBase64 = ( file, cb ) => {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            cb(reader.result)
+        };
+        reader.onerror = function (error) {
+            console.log('Error: ', error);
+        };
+    }
+
+    const handleChange = async (e) =>{
+
+        contentBar.current.style.display = "block"
+        closeBtn.current.style.display = "block"
+
+        await timeout(50) 
+        
+        bar.current.style.width = "100%"
+
+    
+        
+        getBase64(e.target.files[0], (result) => {
+            props.uploadFile(result)
+            console.log(result)
+       });
+       
+    }
+
+  return (
+    <div className="container">
+      <span className="span-input">{props.title}</span>
+      <input id="input" type="file" accept="application/pdf" onChange={handleChange} ref={input}/>
+
+      <div className="bar-buttons">
+        <div id="content-bar" className="progressbar-container" ref={contentBar}>
+          <div id="bar" className="progressbar" ref={bar}></div>
+        </div>
+
+        <button id="close-btn" className="button-cancel" type="button" onClick={handleClick} ref={closeBtn}>
+          x
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default InputFile;
